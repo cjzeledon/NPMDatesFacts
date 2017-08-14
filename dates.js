2db2707ad758989e78fa600e8acfebb7b97ff191
Claudia Zeledon <cjzeledon@gmail.com>
2017-08-14 16:00:34 -0400

    Fixed the seconds portion. Realized it required mathemathical equation to give correct seconds in a day.

--- dates.js
+++ dates.js
@@ -1,10 +1,60 @@
-console.log("I'm absolutely working fine here!");
+let chalk = require("chalk");
+let moment = require("moment");
 
-chalk = require("chalk");
-moment = require("moment");
+let fullTime = moment().format("LLLL");
+let stringfullTime = "It is " + chalk.blue(fullTime) + ".";
+console.log(stringfullTime);
 
-// How to use moment variable, use moment().format();
+let manyDays = moment().format("DDDo");
+let stringManyDays = "It is the " + chalk.magenta(manyDays) + " day of the year.";
+console.log(stringManyDays);
 
-define(['moment'], function (moment) {
-    console.log(moment().format('LLLL'));  // 'Friday, June 24, 2016 1:42 AM'
-});
+function MathSeconds (){
+// let manySeconds = moment().format("SSSSSSSSS");
+// console.log(manySeconds);
+// let stringManySeconds = "It is " + chalk.cyan(manySeconds) + " seconds into the day.";
+// console.log(stringManySeconds);
+
+let hours = moment().format("H");
+  // console.log(hours);
+let minutes = moment().format("m");
+  // console.log(minutes);
+let seconds = moment().format("s");
+  // console.log(seconds);
+
+let calculateSeconds = (((parseFloat(hours) * 60) + parseFloat(minutes))* 60)+ parseFloat(seconds);
+let stringDOUBLECHECKseconds = "It is " + chalk.cyan(calculateSeconds) + " seconds into the day. I'm DOUBLE CHECKED.";
+  console.log(stringDOUBLECHECKseconds);
+
+
+// let checkSECONDS = moment().format("SSS");
+// console.log(checkSECONDS);
+}
+MathSeconds();
+// 24hours * 60 minutes * 60 seconds = total
+
+function YesDaylight (answer){
+  let savingTime = moment().isDST();
+  if (savingTime === true){
+    savingTime = "is";
+  } else {
+    savingTime = "is not";
+  }
+  return savingTime;
+}
+
+  let stringSavingTime = "It " + chalk.green(YesDaylight()) + " during Daylight Savings Time."
+  console.log(stringSavingTime);
+
+
+function LeapYear (leaping){
+  let leapingTime = moment().isLeapYear();
+  if (leapingTime !== true){
+   leapingTime = "is not";
+  } else {
+   leapingTime = "is";
+  }
+  return leapingTime;
+}
+  let stringLeapingTime = "It " + chalk.red(LeapYear()) + " a leap year."
+  console.log(stringLeapingTime);
