8fef56ce4d8481326b04dcacdf89091ca90724c8
Claudia Zeledon <cjzeledon@gmail.com>
2017-08-14 14:25:20 -0400

    Copied and pasted the code from moment document website to see how it works

--- dates.js
+++ dates.js
@@ -1,1 +1,10 @@
 console.log("I'm absolutely working fine here!");
+
+chalk = require("chalk");
+moment = require("moment");
+
+// How to use moment variable, use moment().format();
+
+define(['moment'], function (moment) {
+    console.log(moment().format('LLLL'));  // 'Friday, June 24, 2016 1:42 AM'
+});
